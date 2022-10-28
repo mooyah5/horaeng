4c79688d1256e75fb9a14a912fa2fb820021a1fa
gandi0330 <gandi0330@naver.com>
2022-10-28 09:08:24 +0900

    [feat] api gateway init S07P31C108-102

---
+++ backend/auth-service/src/main/java/com/dool/authservice/controller/AuthController.java
@@ -0,0 +1,2 @@
+package com.dool.authservice.controller;public class AuthController {
+}
---
+++ backend/gateway/src/main/java/com/dool/gateway/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.dool.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request","first-request-header")
+                                        .addResponseHeader("first-response","first-response-header"))
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+}
