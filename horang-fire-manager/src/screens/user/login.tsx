a9407405363ad6a0d4514118002b903a070dc2da
jmlee9707 <jmlee9707@naver.com>
2022-11-01 14:58:39 +0900

    [fix] 병합 에러 주석

--- horang-fire-manager/src/screens/user/login.tsx
+++ horang-fire-manager/src/screens/user/login.tsx
@@ -1,45 +1,46 @@
 import React, {useState} from 'react';
 // import style from './login.sass';
 
-const styles = {
-  title: {
-    textAlign: 'center',
-  },
-  input: {
-    border: 'solid 1px black',
-  } as React.CSSProperties,
-};
+// const styles = {
+//   title: {
+//     textAlign: 'center',
+//   },
+//   input: {
+//     border: 'solid 1px black',
+//   } as React.CSSProperties,
+// };
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState();
-  const emailHandle = (e: any) => {
-    setEmail(e.target.value);
-  };
-  const passwordHandle = (e: any) => {
-    setPassword(e.target.value);
-  };
-  const submitHandle = () => {
-    console.log(email, password);
-  };
-  return (
-    <>
-      <div className="">
-        <div>
-          <p style={styles.title}>로그인</p>
-        </div>
-        <div>
-          <p>이메일</p>
-          <input style={styles.input} type="text" onChange={emailHandle} />
-          <p>비밀번호</p>
-          <input style={styles.input} type="text" onChange={passwordHandle} />
-        </div>
-        <div>
-          <button onClick={submitHandle}>로그인</button>
-        </div>
-      </div>
-    </>
-  );
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState();
+  // const emailHandle = (e: any) => {
+  //   setEmail(e.target.value);
+  // };
+  // const passwordHandle = (e: any) => {
+  //   setPassword(e.target.value);
+  // };
+  // const submitHandle = () => {
+  //   console.log(email, password);
+  // };
+  // return (
+  //   <>
+  //     <div className="">
+  //       <div>
+  //         <p style={styles.title}>로그인</p>
+  //       </div>
+  //       <div>
+  //         <p>이메일</p>
+  //         <input style={styles.input} type="text" onChange={emailHandle} />
+  //         <p>비밀번호</p>
+  //         <input style={styles.input} type="text" onChange={passwordHandle} />
+  //       </div>
+  //       <div>
+  //         <button onClick={submitHandle}>로그인</button>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
+  return <div>dd</div>;
 }
 
 export default Login;
