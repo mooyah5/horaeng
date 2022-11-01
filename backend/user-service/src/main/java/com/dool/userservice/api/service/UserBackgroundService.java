dbc6e0269189a6c76d7370c7ca5ab4769c071269
gandi0330 <gandi0330@naver.com>
2022-11-01 15:24:43 +0900

    Feat : 유저 배경 API 개발 S07P31C108-175

--- backend/user-service/src/main/java/com/dool/userservice/api/service/UserBackgroundService.java
+++ backend/user-service/src/main/java/com/dool/userservice/api/service/UserBackgroundService.java
@@ -1,8 +1,9 @@
 package com.dool.userservice.api.service;
 
+import com.dool.userservice.api.request.BuyBackgroundRequest;
 import com.dool.userservice.db.domain.UserBackground;
 
 public interface UserBackgroundService {
-    Iterable<UserBackground> getUsersBackground();
-    void buyBackground();
+    Iterable<UserBackground> getUsersBackground(String userId);
+    void buyBackground(BuyBackgroundRequest request);
 }
