92a5616d1ded3161bb8a0d4ccb1b4fbf311f0680
정현명 <gandi0330@naver.com>
2022-11-07 15:24:22 +0900

    Feat : 신고 타입 생성

--- backend/social-service/src/main/java/com/dool/socialservice/api/request/CreateReportRequest.java
+++ backend/social-service/src/main/java/com/dool/socialservice/api/request/CreateReportRequest.java
@@ -1,5 +1,7 @@
 package com.dool.socialservice.api.request;
 
+import com.dool.socialservice.db.domain.ReportType;
+import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +9,10 @@ import lombok.Setter;
 @Getter @Setter
 @Data
 public class CreateReportRequest {
+    @NotNull
     private String userId;
+    @NotNull
     private Long diaryId;
+    @NotNull
+    private ReportType reportType;
 }
