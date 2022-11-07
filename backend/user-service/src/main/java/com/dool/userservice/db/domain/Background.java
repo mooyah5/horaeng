2533624036503034a6fa461b895a444cbd1ae4e9
gandi0330 <gandi0330@naver.com>
2022-11-07 15:30:52 +0900

    Feat : Not null 유효성 검사 추가

--- backend/user-service/src/main/java/com/dool/userservice/db/domain/Background.java
+++ backend/user-service/src/main/java/com/dool/userservice/db/domain/Background.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -14,8 +15,11 @@ import javax.persistence.*;
 public class Background {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private Long id;
+    @NotNull
     private String imgUrl;
+    @NotNull
     private Long price;
 
 
