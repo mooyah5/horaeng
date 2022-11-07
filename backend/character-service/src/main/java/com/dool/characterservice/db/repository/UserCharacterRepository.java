78734c217c64345b25b86bc3051439077f2c44ed
박상현 <nomzaxs@gmail.com>
2022-11-07 11:43:50 +0900

    fix : usercharacter null 처리

--- backend/character-service/src/main/java/com/dool/characterservice/db/repository/UserCharacterRepository.java
+++ backend/character-service/src/main/java/com/dool/characterservice/db/repository/UserCharacterRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserCharacterRepository extends JpaRepository<UserCharacter, Long> {
-    UserCharacter findByUserIdAndStatusFalse(String user_id);
+    Optional<UserCharacter> findByUserIdAndStatusFalse(String user_id);
 }
