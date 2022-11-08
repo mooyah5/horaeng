d71b005bc5f78faa2c119b2ac85f1ce81f8122a8
gandi0330 <gandi0330@naver.com>
2022-11-08 16:40:43 +0900

    hotfix : patch->put

--- backend/social-service/src/main/java/com/dool/socialservice/api/client/CharacterMissionServiceClient.java
+++ backend/social-service/src/main/java/com/dool/socialservice/api/client/CharacterMissionServiceClient.java
@@ -3,10 +3,11 @@ package com.dool.socialservice.api.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(name="character-service")
 public interface CharacterMissionServiceClient {
-    @PatchMapping("/character-service/character-mission/{CMId}")
+    @PutMapping("/character-service/character-mission/{CMId}")
     void completeMission(@PathVariable("CMId") Long CMId);
 
 }
