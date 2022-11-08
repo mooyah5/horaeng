c5ea3320d414a9d68d9ba45fa12924f827842891
박상현 <nomzaxs@gmail.com>
2022-11-08 10:54:40 +0900

    fix : usercharacter

--- backend/character-service/src/main/java/com/dool/characterservice/api/controller/MIssionController.java
+++ backend/character-service/src/main/java/com/dool/characterservice/api/controller/MIssionController.java
@@ -1,37 +1,62 @@
 package com.dool.characterservice.api.controller;
 
+import com.dool.characterservice.api.request.MissionRequest;
 import com.dool.characterservice.api.response.MissionResponseDto;
 import com.dool.characterservice.api.service.MissionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin("*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/character-service/mission")
 public class MIssionController {
-
+    private static final Object SUCCESS = "SUCCESS";
+    private static final Object FAIL = "FAIL";
     private final MissionService missionService;
 
     @GetMapping
-    private ResponseEntity<List<MissionResponseDto>> getMissionList() {
+    private ResponseEntity<?> getMissionList() {
         List<MissionResponseDto> missionResponseDtoList = missionService.getMission();
 
         return ResponseEntity.status(200).body(missionResponseDtoList);
     }
 
     @GetMapping("{id}")
-    private ResponseEntity getMission(@PathVariable("id") Long id){
+    private ResponseEntity<?> getMission(@PathVariable("id") Long id){
         MissionResponseDto missionResponseDto = missionService.missionDetail(id);
 
         return ResponseEntity.status(200).body(missionResponseDto);
     }
 
     @PostMapping
-    private ResponseEntity postMission(){
+    private ResponseEntity<?> postMission(@RequestBody MissionRequest missionRequest){
+        Map<String, Object> result = new HashMap<>();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        try {
+            MissionResponseDto responseDto = missionService.creat(missionRequest);
+
+            if(responseDto != null){
+                result.put("mission", responseDto);
+            }
+
+            status = HttpStatus.OK;
+            result.put("message", SUCCESS);
+        }catch (Exception e){
+            result.put("message", FAIL);
+        }
+
+        return new ResponseEntity<>(result, status);
+    }
+
+    @DeleteMapping("{id}")
+    private ResponseEntity<?> del(@PathVariable("id") Long id){
 
         return null;
     }
--- backend/character-service/src/main/java/com/dool/characterservice/api/service/MissionService.java
+++ backend/character-service/src/main/java/com/dool/characterservice/api/service/MissionService.java
@@ -1,5 +1,6 @@
 package com.dool.characterservice.api.service;
 
+import com.dool.characterservice.api.request.MissionRequest;
 import com.dool.characterservice.api.response.MissionResponseDto;
 
 import java.util.List;
@@ -7,4 +8,5 @@ import java.util.List;
 public interface MissionService {
     MissionResponseDto missionDetail(Long id);
     List<MissionResponseDto> getMission();
+    MissionResponseDto creat(MissionRequest missionRequest);
 }
