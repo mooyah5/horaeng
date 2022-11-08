254d23f455ca9439fd88fb4d45643efed835896b
gandi0330 <gandi0330@naver.com>
2022-11-08 15:26:57 +0900

    hotfix : uri 수정

--- backend/social-service/src/main/java/com/dool/socialservice/api/client/CharacterMissionServiceClient.java
+++ backend/social-service/src/main/java/com/dool/socialservice/api/client/CharacterMissionServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="character-service")
 public interface CharacterMissionServiceClient {
-    @PatchMapping("/character-serice/character-mission/{CMId}")
+    @PatchMapping("/character-service/character-mission/{CMId}")
     void completeMission(@PathVariable("CMId") Long CMId);
 
 }
