47867fdc253b2e693405fde7f066932d8246af18
박상현 <nomzaxs@gmail.com>
2022-11-14 15:12:27 +0900

    Revert "test : rewrite path"
    This reverts commit 8f4cc7abf115d7b035577ff115ead8c9b5c5e9e6.
    
    # Conflicts:
    #	backend/character-service/src/main/java/com/dool/characterservice/api/controller/CharacterController.java

---
+++ backend/character-service/src/main/java/com/dool/characterservice/api/controller/CharacterController.java
@@ -0,0 +1,28 @@
+package com.dool.characterservice.api.controller;
+
+import com.dool.characterservice.api.response.CharactersResponseDto;
+import com.dool.characterservice.api.service.CharactersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/character-service/character")
+public class CharacterController {
+
+    private final CharactersService characterService;
+
+    @GetMapping
+    private ResponseEntity<List<CharactersResponseDto>> findAll(){
+        List<CharactersResponseDto> charactersList = characterService.getCharacters();
+
+        return ResponseEntity.status(200).body(charactersList);
+    }
+}
