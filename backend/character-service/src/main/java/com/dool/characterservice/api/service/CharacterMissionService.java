5084f5d003d6bc03ece9048c7ac1e6b3f2b21afb
박상현 <nomzaxs@gmail.com>
2022-11-18 16:27:57 +0900

    Fix : 메인, 공통 미션별 수행여부 확인 1. 미션 종류에 따른 return 수정 2. 캐릭터 미션 서비스의 리펙토링

--- backend/character-service/src/main/java/com/dool/characterservice/api/service/CharacterMissionService.java
+++ backend/character-service/src/main/java/com/dool/characterservice/api/service/CharacterMissionService.java
@@ -6,7 +6,8 @@ import com.dool.characterservice.db.domain.CharacterMission;
 import java.util.List;
 
 public interface CharacterMissionService {
-    boolean todayClear(Long user_character_id);
+    boolean todayMainClear(Long user_character_id);
+    boolean todayCommonClear(Long user_character_id);
     CharacterMission postMission(Long user_character_id);
     Long countMission(Long user_character_id);
     boolean complete(Long CMId);
